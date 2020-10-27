a492001b8c83aea2b0c702ab8b1d4254d57c5e90
Kun Kim <kunhkim93@gmail.com>
2020-10-27 15:27:45 +0900

    add inputrc

--- install.sh
+++ install.sh
@@ -3,11 +3,13 @@
 # Clean up previous links if exists
 rm -v ~/.config/fish/config.fish
 rm -v ~/.config/nvim/init.vim
-rm -v ~/.gitconfig
 rm -v ~/.tmux.conf
+rm -v ~/.gitconfig
+rm -v ~/.inputrc
 
 # Create links
 ln -sv ~/.dotfiles/config.fish $HOME/.config/fish/config.fish
 ln -sv ~/.dotfiles/init.vim ~/.config/nvim/init.vim
-ln -sv ~/.dotfiles/gitconfig ~/.gitconfig
 ln -sv ~/.dotfiles/tmux.conf $HOME/.tmux.conf
+ln -sv ~/.dotfiles/gitconfig ~/.gitconfig
+ln -sv ~/.dotfiles/inputrc $HOME/.inputrc
